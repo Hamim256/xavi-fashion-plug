7feb59c9383ffc20b008ffc14a8a8000fe635445
Hamim256 <sekayihamim5@gmail.com>
2026-08-17 11:30:04 +0300

    Update Supabase URL to remove endpoint path

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-const SUPABASE_URL = "https://wovyhrpehywmxdhjibbq.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://wovyhrpehywmxdhjibbq.supabase.co";
+
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_xoRcJtQ1_HN-F_XbwdXD0A_H5jzV9MD";
 
 const supabaseClient = window.supabase.createClient(
